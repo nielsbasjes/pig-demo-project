7a878859005c03a2667f6be8e47b65363dc149ac
Niels Basjes <niels@basjes.nl>
2015-02-11 15:54:03 +0100

    Cleanup unit test code

--- src/test/java/pigtests/TestPigDemo_PigUnit.java
+++ src/test/java/pigtests/TestPigDemo_PigUnit.java
@@ -9,11 +9,10 @@ import java.io.IOException;
 public class TestPigDemo_PigUnit {
 
   @Test
-  public void testPigScript() throws IOException, ParseException {
-    String[] input = {"One\tTwo\tThree Four"};
-    System.out.println("Working Directory = " +
-            System.getProperty("user.dir"));
-
+  public void testPigScriptInCodeInput() throws IOException, ParseException {
+    String[] input = {
+            "One\tTwo\tThree Four"
+    };
     String[] arguments = {
     };
 
@@ -27,6 +26,23 @@ public class TestPigDemo_PigUnit {
             "(One,Two,Four)",
     };
     test.assertOutput("Clicks", input, "Stuff", output);
+  }
+
+
+  @Test
+  public void testPigScriptIncludingLoader() throws IOException, ParseException {
+    String[] arguments = {
+            "ACCESS_LOGFORMAT='%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"'",
+            "ACCESS_LOGPATH='src/test/resources/'"
+    };
+
+    PigTest test = new PigTest("src/main/pig/demo.pig", arguments);
+    String[] output = {
+            "(/1-500e-KWh?FoO=bAr%20BaR,PowerTick,bAr)",
+            "(/1-500e-KWh?FoO=bAr%20BaR,PowerTick,BaR)",
+    };
+
+    test.assertOutput("Stuff", output);
   }
 
 }
